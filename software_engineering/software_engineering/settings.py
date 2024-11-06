d3c660e036fc89639639fe120921f0b42c65a4d7
IgnacyBerent <2gb02ignac@gmail.com>
2024-11-06 10:15:30 +0100

    Setting up docker-compose and postgresql

--- software_engineering/software_engineering/settings.py
+++ software_engineering/software_engineering/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = "software_engineering.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "pgdb",
+        "PORT": "5432",
     }
 }
 
