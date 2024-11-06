8727369c2c22e030fb8ae8d282f11985e4c1f9f2
IgnacyBerent <2gb02ignac@gmail.com>
2024-11-06 12:22:43 +0100

    Adding Product and Customer to the database, adding admin.sites

--- software_engineering/myapp/models.py
+++ software_engineering/myapp/models.py
@@ -1,7 +1,33 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0.01)])
     available = models.BooleanField(default=True)
+
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('New', 'New'),
+        ('In Process', 'In Process'),
+        ('Sent', 'Sent'),
+        ('Completed', 'Completed'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    date = models.DateField()
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES)
+
+    def total_price(self):
+        return sum([product.price for product in self.products.all()])
+    
+    def can_be_fullfilled(self):
+        return all([product.available for product in self.products.all()])
