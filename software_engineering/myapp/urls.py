57ffa10a3b3b8174120cb79567246767d82aa189
IgnacyBerent <2gb02ignac@gmail.com>
2024-12-04 10:28:30 +0100

    finishing Problem list 7

--- software_engineering/myapp/urls.py
+++ software_engineering/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, CustomerViewSet, OrderViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 router = DefaultRouter()
@@ -9,5 +10,7 @@ router.register(r"customers", CustomerViewSet, basename="customer")
 router.register(r"orders", OrderViewSet, basename="order")
 
 urlpatterns = [
-    path("api/", include(router.urls)),
+    path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- software_engineering/myapp/views.py
+++ software_engineering/myapp/views.py
@@ -1,19 +1,24 @@
 import json
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAdminOrReadOnly
 from .serializers import ProductSerializer, CustomerSerializer, OrderSerializer
 from .models import Product, Customer, Order
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
