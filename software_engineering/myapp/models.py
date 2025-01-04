ca1881385468388ea23a5327443c934f4767d5eb
IgnacyBerent <2gb02ignac@gmail.com>
2025-01-04 16:27:00 +0100

    temporary changes to models from step 11

--- software_engineering/myapp/models.py
+++ software_engineering/myapp/models.py
@@ -1,23 +1,26 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
+
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0.01)])
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     available = models.BooleanField(default=True)
+
 
 class Customer(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     address = models.CharField(max_length=255)
+
 
 class Order(models.Model):
     STATUS_CHOICES = [
-        ('New', 'New'),
-        ('In Process', 'In Process'),
-        ('Sent', 'Sent'),
-        ('Completed', 'Completed'),
+        ("New", "New"),
+        ("In Process", "In Process"),
+        ("Sent", "Sent"),
+        ("Completed", "Completed"),
     ]
 
     id = models.AutoField(primary_key=True)
@@ -28,6 +31,6 @@ class Order(models.Model):
 
     def total_price(self):
         return sum([product.price for product in self.products.all()])
-    
+
     def can_be_fullfilled(self):
         return all([product.available for product in self.products.all()])
