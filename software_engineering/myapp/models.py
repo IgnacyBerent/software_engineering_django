35455ebece473a2b5acfd40dd950ba4c7cccf002
IgnacyBerent <2gb02ignac@gmail.com>
2025-01-04 16:31:54 +0100

    Reverting changes from step 11

--- software_engineering/myapp/models.py
+++ software_engineering/myapp/models.py
@@ -5,13 +5,15 @@ from django.core.validators import MinValueValidator
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(
+        max_digits=5, decimal_places=2, validators=[MinValueValidator(0.01)]
+    )
     available = models.BooleanField(default=True)
 
 
 class Customer(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     address = models.CharField(max_length=255)
 
 
