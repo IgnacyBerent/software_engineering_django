a59d9faebe04616a617e3f8e49477ad0d7d5d186
IgnacyBerent <2gb02ignac@gmail.com>
2025-01-17 12:04:34 +0100

    Fixing introduced bug

--- software_engineering/myapp/serializers.py
+++ software_engineering/myapp/serializers.py
@@ -7,11 +7,6 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = "__all__"
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation["price"] = 0
-        return representation
-
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
